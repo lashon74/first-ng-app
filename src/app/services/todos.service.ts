ed1a971f02a0ca519f9671f314b76f9498d37eb7
Lashon Hudson <lashonhud@gmail.com>
2025-04-29 21:27:03 -0400

    Angular services using them through out the entire application

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
-
-  constructor() { }
+  todoItems: Array<Todo> = [
+    {
+      title: 'groceries',
+      userId: 74,
+      id: 0,
+      completed: false,
+    },
+    {
+      title: 'car wash',
+      userId: 1,
+      id: 1,
+      completed: false,
+    },
+  ];
+  constructor() {}
 }
