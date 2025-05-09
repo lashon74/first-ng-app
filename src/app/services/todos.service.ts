2c4111a041db7fc42bfc75688f1bbf25ca9f6b32
Lashon Hudson <lashonhud@gmail.com>
2025-05-08 21:16:19 -0400

    Http calls in angular services I added the provided then injected it into the todos service which allows me to use it within the todos component

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,23 +1,28 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-  todoItems: Array<Todo> = [
-    {
-      title: 'groceries',
-      userId: 74,
-      id: 0,
-      completed: false,
-    },
-    {
-      title: 'car wash',
-      userId: 1,
-      id: 1,
-      completed: false,
-    },
-  ];
-  constructor() {}
+  http = inject(HttpClient);
+  // todoItems: Array<Todo> = [
+  //   {
+  //     title: 'groceries',
+  //     userId: 74,
+  //     id: 0,
+  //     completed: false,
+  //   },
+  //   {
+  //     title: 'car wash',
+  //     userId: 1,
+  //     id: 1,
+  //     completed: false,
+  //   },
+  // ];
+  getTodosFromApi() {
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get<Array<Todo>>(url);
+  }
 }
